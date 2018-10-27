8b1c2195a8638c0a4ccc8dfa6e732a88c4b5079c
ddevcap <daan@devcap.nl>
2018-10-27 23:36:37 +0200

    more pointers

--- forecast.go
+++ forecast.go
@@ -1,10 +1,10 @@
 package checkwx
 
 type Forecast struct {
-	Timestamp       ForecastTimestamp `json:"timestamp,omitempty,omitempty"`
-	Clouds          []Cloud           `json:"clouds,omitempty"`
-	Conditions      []Condition       `json:"conditions"`
-	Visibility      Visibility        `json:"visibility,omitempty"`
-	Wind            Wind              `json:"wind,omitempty"`
+	Timestamp       *ForecastTimestamp `json:"timestamp,omitempty,omitempty"`
+	Clouds          []*Cloud           `json:"clouds,omitempty"`
+	Conditions      []*Condition       `json:"conditions"`
+	Visibility      *Visibility        `json:"visibility,omitempty"`
+	Wind            *Wind              `json:"wind,omitempty"`
 	ChangeIndicator string            `json:"change_indicator,omitempty"`
 }
